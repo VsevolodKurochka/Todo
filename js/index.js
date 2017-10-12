0ffe544a44d173f9ea60fe49eb7b5e88aab51510
VsevolodKurochka <dramdoobas123456@gmail.com>
2017-10-12 19:23:01 +0300

    [0.0.3]

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+ function log(log){
+	console.log(log);
+}
 let data = {
 	title: 'List to run!',
 	todo: [
@@ -12,43 +15,74 @@ let data = {
 		}
 	]
 }
-class TodoElement extends React.Component{
-	constructor(props){
-		super(props);
-	}
-	render(){
-		return(
-			<li className="list-group-item">{this.props.text}</li>
-		)
-	}
-}
 class Todo extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			data: this.props.data
+			title: this.props.data.title,
+			items: this.props.data.todo,
+			inputValue: 'I need to...'
 		}
+		this.addToList = this.addToList.bind(this);
+		this.changeValue = this.changeValue.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+	addToList(e){
+		e.preventDefault();
+		let newArray = this.state.items.slice();
+		newArray.push({
+			name: this.state.inputValue
+		})
+		this.setState({
+			items: newArray
+		})
+	}
+	changeValue(e){
+		this.setState({
+			inputValue: e.target.value
+		})
+	}
+	deleteItem(item){
+		let currentItems = this.state.items;
+		currentItems.splice(item, 1);
+		this.setState({
+			items: currentItems
+		})
 	}
 	render(){
+		// <span><i className="material-icons">close</i></span>
 		return(
 			<div className="jumbotron">
 				<div className="container">
+					<h1 className="mb-5">React Todo Application</h1>
 					<div className="row">
 						<div className="col-12 col-sm-6">
-
+							<h3 className="mb-2">{this.state.title}</h3>
+							<ul className="list-group">
+								{this.state.items.map(function(item, i){
+									return(
+										<li className="list-group-item list-group-item_flex" key={i}>
+											<span><span className="font-weight-bold">{i+1}</span>. {item.name}</span>
+											<span onClick={this.deleteItem}><i className="material-icons">close</i></span>
+										</li>
+									)
+								}.bind(this))}
+							</ul>
 						</div>
 						<div className="col-12 col-sm-6">
-							<h1 className="mb-3">{this.state.data.title}</h1>
-							<ul className="list-group">
-								{this.state.data.todo.map(function(item){
-									return <TodoElement key={item.name} text={item.name} />
-								})}
-							</ul>
+							<h3 className="mb-2">You can add more items to your list.</h3>
+							<form onSubmit={this.addToList}>
+								<div className="input-group">
+									<input type="text" className="form-control" placeholder="Add..." value={this.state.inputValue} onChange={e => this.changeValue(e)}/>
+									<span className="input-group-btn">
+										<button onClick={this.addToList} className="btn btn-primary" type="button">Add item</button>
+									</span>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
 			</div>
-			
 		)
 	}
 }
